d3e9c5205a8ac8163fc4392d617eff3424ee6712
emilbratt <emilbratt@gmail.com>
2023-09-17 17:51:20 +0200

    Extracting logic out of main.js. Everything except from the part that loads the chess-board-html. At this point, main.js serves only as a simple entrypoint for the application.
    The reason for this is that I will try to write some tests.
    Writing tests will require that I can access relevant stuff like variables, classes, functions etc..
    Since loading main.js automatically loads the board (which I do not want if running tests) that means that I can not load main.js..
    Solution: I move stuff we need to access out of main.js, sorry main.js..
    
    Maybe there is a better way, would love to get feedback.

--- src/js/board.js
+++ src/js/board.js
@@ -1,3 +1,8 @@
+const board = document.getElementById("board");
+let currentPosition = [];
+let tilesCreated = false;
+
+
 function createBackline(y, color) {
     return [
         new Rook(0, y, color),
@@ -41,4 +46,63 @@ function createStartingPosition() {
     }
     console.log(newBoard);
     return newBoard;
+}
+
+function createIdString(row, col) {
+    return `${row}-${col}`;
+}
+
+function createTilesHTML() {
+    for (let row = 0; row < currentPosition.length; row++) {
+        for (let col = 0; col < currentPosition[0].length; col++) {
+            tile = document.createElement("div");
+            tile.id = createIdString(row, col);
+            tile.classList.add("tile");
+            if (row % 2) tile.classList.add("odd");
+            tile.addEventListener("mousedown", () => {
+                if (currentPosition[row][col]) {
+                    console.log(currentPosition[row][col].getPossibleMoves());
+                }
+            });
+            board.append(tile);
+        }
+    }
+    tilesCreated = true;
+}
+
+function getTileElement(row, col) {
+    return document.getElementById(`${row}-${col}`);
+}
+
+function drawCurrentPosition() {
+    /*
+     * A 2D array holds the position data for each square.
+     * Each element in the array contains one of two options:
+     *
+     *      If there is no piece, the element is null.
+     *
+     *      If there is a piece, the element is an instance of a subclass
+     *      of Piece corresponding to the piece in the square.
+     *
+     * Each instance of Piece has its own getPossibleMoves method.
+     * This method is attached to an event listener.
+     */
+
+    if (!tilesCreated) {
+        createTilesHTML();
+    }
+    for (let row = 0; row < currentPosition.length; row++) {
+        for (let col = 0; col < currentPosition[0].length; col++) {
+            currentPiece = currentPosition[row][col];
+            if (currentPiece) {
+                const currentTile = getTileElement(row, col);
+                const pieceText = currentPiece.symbol;
+                const IS_WHITE = currentPiece.color === "white";
+                const IS_BLACK = !IS_WHITE;
+                currentTile.textContent = pieceText;
+                currentTile.classList.toggle("white", IS_WHITE);
+                currentTile.classList.toggle("black", IS_BLACK);
+            }
+        }
+    }
 }
