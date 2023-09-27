e57d296f82fa8cabb08b88ed82ec7165e5dea88d
emilbratt <emilbratt@gmail.com>
2023-09-27 09:59:32 +0200

    Clean-up main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 function initApp() {
     currentPosition = createStartingPosition();
-    let p = currentPosition[0][0];
     createTilesHTML();
     drawCurrentPosition();
-
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
